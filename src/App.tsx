83eb7eaec54287378c74eca3ffde8cdac13ca15c
Gustavo <gustavogmfarias@gmail.com>
2022-12-22 11:10:44 -0300

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,17 @@
+import { ThemeProvider } from "styled-components";
+import { Button } from "./components/Button";
+import { defaultTheme } from "./styles/themes/default";
+
 export function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <h1>Hello World</h1>
+
+      <Button variant="primary" />
+      <Button variant="secondary" />
+      <Button variant="success" />
+      <Button variant="danger" />
+      <Button />
+    </ThemeProvider>
+  );
 }

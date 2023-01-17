bb0fd78c8137033e1d541a96f42d5f45795a76c0
Gustavo <gustavogmfarias@gmail.com>
2023-01-17 09:01:14 -0300

    .

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -15,7 +15,9 @@ export const GlobalStyle = createGlobalStyle`
 
 body {
     background-color: ${(props) => props.theme["gray-900"]};
-    color: ${(props) => props.theme["gray-300"]}
+    color: ${(props) => props.theme["gray-300"]};
+    -webkit-font-smoothing: antialiased;
+
 }
 
 body, input, textarea {

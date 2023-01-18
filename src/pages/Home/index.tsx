8c1fe9d358b8f4f9af46ecb43ed47dc82084e178
Gustavo <gustavogmfarias@gmail.com>
2023-01-18 11:23:06 -0300

    .

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,13 @@
 import { Play } from "phosphor-react";
-import { CountdownContainer, FormContainer, HomeContainer } from "./styles";
+import {
+    CountdownContainer,
+    FormContainer,
+    HomeContainer,
+    MinutesAmountInput,
+    Separator,
+    StartCountdownButton,
+    TaskInput,
+} from "./styles";
 
 export function Home() {
     return (
@@ -8,21 +16,29 @@ export function Home() {
                 <FormContainer>
                     {" "}
                     <label htmlFor="task">Vou trabalhar em</label>
-                    <input id="task" type=""></input>
+                    <TaskInput
+                        id="task"
+                        type=""
+                        placeholder="Dê um nome para o seu projeto"
+                    ></TaskInput>
                     <label htmlFor="minutesAmount"> durante</label>
-                    <input type="number" id="minutesAmount" />
+                    <MinutesAmountInput
+                        type="number"
+                        id="minutesAmount"
+                        placeholder="00"
+                    />
                     <span>minutos.</span>
                 </FormContainer>
                 <CountdownContainer>
                     <span>0</span>
                     <span>0</span>
-                    <span>:</span>
+                    <Separator>:</Separator>
                     <span>0</span>
                     <span>0</span>
                 </CountdownContainer>
-                <button type="submit">
+                <StartCountdownButton type="submit">
                     <Play size={24} /> Começar
-                </button>
+                </StartCountdownButton>
             </form>
         </HomeContainer>
     );
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -25,8 +25,61 @@ export const FormContainer = styled.div`
     font-weight: bold;
     flex-wrap: wrap;
 `;
+
+export const TaskInput = styled.input``;
+export const MinutesAmountInput = styled.input``;
+
 export const CountdownContainer = styled.div`
     font-family: "Roboto Mono", monospace;
     font-size: 10rem;
     line-height: 8rem;
+    color: ${(props) => props.theme["gray-100"]};
+
+    display: flex;
+    gap: 1rem;
+
+    span {
+        background: ${(props) => props.theme["gray-700"]};
+        padding: 2rem 1rem;
+        border-radius: 8px;
+    }
+`;
+
+export const Separator = styled.div`
+    padding: 2rem 0;
+
+    color: ${(props) => props.theme["green-500"]};
+
+    width: 4rem;
+    overflow: hidden;
+    display: flex;
+    justify-content: center;
+`;
+
+export const StartCountdownButton = styled.button`
+    width: 100%;
+    border: 0;
+    padding: 1rem;
+    border-radius: 8px;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    gap: 0.5rem;
+    font-weight: bold;
+
+    cursor: pointer;
+
+    background: ${(props) => props.theme["green-500"]};
+    color: ${(props) => props.theme["gray-100"]};
+
+    &:disabled {
+        opacity: 0.7;
+        cursor: not-allowed;
+    }
+
+    &:not(:disabled):hover {
+        background: ${(props) => props.theme["green-700"]};
+    }
 `;

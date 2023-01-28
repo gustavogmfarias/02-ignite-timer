81fb66b704bdd2b2a44c5bfbaae416769bac8ac7
Gustavo <gustavogmfarias@gmail.com>
2023-01-28 11:46:37 -0300

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,18 @@
-import { BrowserRouter } from "react-router-dom";
 import { ThemeProvider } from "styled-components";
+import { BrowserRouter } from "react-router-dom";
 import { Router } from "./Router";
+
 import { GlobalStyle } from "./styles/global";
 import { defaultTheme } from "./styles/themes/default";
 
 export function App() {
     return (
-        <BrowserRouter>
-            <ThemeProvider theme={defaultTheme}>
+        <ThemeProvider theme={defaultTheme}>
+            <BrowserRouter>
                 <Router />
-                <GlobalStyle />
-            </ThemeProvider>
-        </BrowserRouter>
+            </BrowserRouter>
+
+            <GlobalStyle />
+        </ThemeProvider>
     );
 }
